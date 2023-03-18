e8852d6f463cb88558eb0cb62aa291cfb8d8872d
Dzmitry-Svi <dima.svitich@yandex.ru>
2023-03-18 16:07:24 +0300

    Первое упражнения 3-го семинара

---
+++ Task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает координаты точки (Х, У). причем Х и У не равны нулю и выдаёт номер четверти плоскости, в которой находится//
+Console.Clear();
+Console.WriteLine("Введите х: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите у: ");
+double y = Convert.ToDouble(Console.ReadLine());
+if(x > 0 && y > 0)
+  Console.WriteLine("1");
+else if(x < 0 && y > 0)
+  Console.WriteLine("2");
+else if(x < 0 && y < 0)
+  Console.WriteLine("3");    
+else 
+  Console.WriteLine("4");
