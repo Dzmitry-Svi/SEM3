4a16731543acbedb5325dc16c83c62a3e9f9577e
Dzmitry-Svi <dima.svitich@yandex.ru>
2023-03-18 16:47:36 +0300

    Третье упражнения 3-го семинара

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая находит координаты двух точек и находит расстояние между ними в 2Dпространстве//
+
+Console.Clear();
+Console.WriteLine("Введите первую координату х1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите первую координату х2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите первую координату y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите первую координату y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2,2)));
