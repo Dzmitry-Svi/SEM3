789f15656681f2cb92a5f2e39ce60d57cc2d17b9
Dzmitry-Svi <dima.svitich@yandex.ru>
2023-03-18 17:08:05 +0300

    4-е упражнения 3-го семинара

---
+++ Task4/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая на вход принимает число N и выдаёт таблицу квадратов чисел от 1 до N//
+
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= N; i++)
+  Console.Write($"{i*i} ");
